410571433232f2829e46ae7dd3b41c8c731fd9cd
Aman Lal <amanlal@syn-2603-8002-c9f0-8010-0000-0000-0000-1764.res6.spectrum.com>
2026-08-26 21:34:01 -0700

    feat: Read bytes sent across TCP server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -9,7 +9,7 @@ func main(){
 	// Start a TCP server on port 8080
 	listener, err := net.Listen("tcp", ":8080")
 	if err != nil {
-		fmt.Println("Error starting server:", err)
+		fmt.Println("Error starting server: ", err)
 		return
 	}
 	defer listener.Close()
@@ -19,10 +19,19 @@ func main(){
 	// Accept incoming connections
 	conn, err := listener.Accept()
 	if err != nil {
-		fmt.Println("Error accepting connection:", err)
+		fmt.Println("Error accepting connection: ", err)
 		return
 	}
 	defer conn.Close()
 
 	fmt.Println("Client connected")
+	
+	buffer := make([]byte, 1024)
+
+	n, err := conn.Read(buffer)
+	if (err != nil) {
+		fmt.Println("Error reading message: ", err)
+		return
+	}
+	fmt.Println("Received:", string(buffer[:n]))
 }
