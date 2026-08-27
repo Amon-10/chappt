2abe664ce014ef08818d6bde4491bc59937e21da
Aman Lal <amanlal@syn-2603-8002-c9f0-8010-0000-0000-0000-1764.res6.spectrum.com>
2026-08-26 20:01:38 -0700

    feat: Create server that starts TCP server and accepts incoming connections

---
+++ cmd/server/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func main(){
+	// Start a TCP server on port 8080
+	listener, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		fmt.Println("Error starting server:", err)
+		return
+	}
+	defer listener.Close()
+	
+	fmt.Println("Server listening on :8080")
+
+	// Accept incoming connections
+	conn, err := listener.Accept()
+	if err != nil {
+		fmt.Println("Error accepting connection:", err)
+		return
+	}
+	defer conn.Close()
+
+	fmt.Println("Client connected")
+}
