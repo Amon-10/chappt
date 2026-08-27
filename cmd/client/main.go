de4f4ea80b404b974890ddd9552d7bd53f7be00e
Aman Lal <amanlal@syn-2603-8002-c9f0-8010-0000-0000-0000-1764.res6.spectrum.com>
2026-08-26 21:33:08 -0700

    feat: Send message as bytes to across TCP server

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -8,10 +8,16 @@ import (
 func main(){
 	conn, err := net.Dial("tcp", "localhost:8080")
 	if (err != nil) {
-		fmt.Println("Error connecting to server", err)
+		fmt.Println("Error connecting to server: ", err)
 		return
 	}
 	defer conn.Close()
 
 	fmt.Println("Connected to server")
+
+	_, err = conn.Write([]byte("hello"))
+	if (err != nil) {
+		fmt.Println("Error sending messages: ", err)
+		return
+	}
 }
