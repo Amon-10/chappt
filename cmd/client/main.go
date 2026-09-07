f53fed7e520dfac54d2792c0cf866a7875bc1f14
Aman Lal <amanlal@syn-2603-8002-c9f0-8010-0000-0000-0000-1764.res6.spectrum.com>
2026-09-07 12:47:05 -0700

    docs: add comments

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -6,15 +6,18 @@ import (
 )
 
 func main(){
+	// Initiate connection to listening tcp server
 	conn, err := net.Dial("tcp", "localhost:8080")
 	if (err != nil) {
 		fmt.Println("Error connecting to server: ", err)
 		return
 	}
-	defer conn.Close()
+	defer conn.Close() // defer - run when surrounding function returns
 
 	fmt.Println("Connected to server")
 
+	// .Write - takes slice of bytes and returns length of the bytes and error object if exists
+	// assign returned length of bytes to _(the blank identifier) where it gets thrown away as it is not needed.
 	_, err = conn.Write([]byte("hello"))
 	if (err != nil) {
 		fmt.Println("Error sending messages: ", err)
